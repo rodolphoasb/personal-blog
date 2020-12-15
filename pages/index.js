637c233a59825541072e05b016775d78ffe3fde5
rodolphoasb <bravorodolpho@gmail.com>
2020-12-15 13:06:22 -0300

    chore: installed sanity

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ export default function IndexPage() {
   return (
     <div>
       <Nav />
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
-          Next.js + Tailwind CSS 2.0
+      <div className='py-20'>
+        <h1 className='text-5xl text-center text-blue-700 dark:text-gray-100'>
+          Next.js and Tailwind CSS 2.0
         </h1>
       </div>
     </div>
