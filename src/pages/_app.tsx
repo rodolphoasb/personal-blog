5b121a05473508946312bc5b15088995be913c12
Rodolpho Bravo <bravorodolpho@gmail.com>
2021-09-21 10:10:58 -0400

    Add leaflet, react-leaflet and configured the dynamicImport of the Map component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,13 +7,16 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Boilerplate</title>
+        <title>Build.</title>
         <link rel="shortcut icon" href="" />
         <link rel="apple-touch-icon" href="" />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossOrigin=""
         />
+        <meta name="description" content="My Personal Blog" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
